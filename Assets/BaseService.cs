099385dd25285ed1859d4146f0212585d73e9c94
Eddy Boughioul <eddyboughioul@hotmail.fr>
2021-01-14 18:34:13 +0100

    update gitignore

---
+++ Assets/BaseService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestLibrary;
+using UnityEngine;
+
+namespace Assets
+{
+    public class BaseService
+    {
+        string url = "http://192.168.1.110:55555";
+        HubConnection connection;
+
+        public event EventHandler<User> UserLoaded;
+        public event EventHandler<UserNoEnum> UserNoEnumLoaded;
+
+        public BaseService()
+        {
+            connection = new HubConnectionBuilder()
+               .WithUrl($"{url}/TestHub")
+               .WithAutomaticReconnect()
+               .Build();
+
+            Start();
+        }
+
+        async Task Start()
+        {
+            await connection.StartAsync();
+
+            connection.On<User>("UserLoaded", (result) =>
+            {
+                Debug.Log("User");
+                if (UserLoaded != null)
+                {
+                    UserLoaded(this, result);
+                }
+                //text = $"{result.Name} {result.Age} {result.Status}";
+            });
+
+            connection.On<UserNoEnum>("UserNoEnumLoaded", (result) =>
+            {
+                Debug.Log("UserNoEnum");
+                if (UserNoEnumLoaded != null)
+                {
+                    UserNoEnumLoaded(this, result);
+                }
+                // 
+            });
+        }
+
+        public async Task GetUser()
+        {
+            await connection.InvokeAsync("Get");
+        }
+
+        public async Task GetUserNoEnum()
+        {
+            await connection.InvokeAsync("GetNoEnum");
+        }
+
+    }
+}
---
+++ Assets/CallService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+using TestLibrary;
+using System;
+using UnityEngine.UI;
+using Assets;
+
+public class CallService : MonoBehaviour
+{
+
+   
+    public Text guiText;
+    string text = "Wait for call";
+    BaseService service;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        service = new BaseService();
+        service.UserLoaded += Service_UserLoaded;
+        service.UserNoEnumLoaded += Service_UserNoEnumLoaded;
+    }
+
+    private void Service_UserNoEnumLoaded(object sender, UserNoEnum result)
+    {
+        text = $"{result.Name} {result.Age}";
+    }
+
+    private void Service_UserLoaded(object sender, User result)
+    {
+        text = $"{result.Name} {result.Age} {result.Status}";
+    }
+
+    public async void GetUser()
+    {
+        await service.GetUser();
+    }
+
+    public async void GetUserNoEnum()
+    {
+        await service.GetUserNoEnum();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (guiText?.text != text)
+        {
+            guiText.text = text;
+        }
+    }
+}
